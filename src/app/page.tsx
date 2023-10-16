2b031bbe1d0b126f6a47bc0c3bfc23963100af5a
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-16 21:32:25 +0530

    adding some tailwind in all cs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import C1 from './components/c1'
+import C2 from './components/c2'
+import C3 from './components/c3'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      this is the main page
-    </main>
+    <div>
+      <C1/>
+      <C2/>
+      <C3/>
+    </div>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   this is the main page
+    // </main>
   )
 }
