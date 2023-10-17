4b10d09b2e5e50b4d994c38a589c4a773d44493d
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-17 19:00:40 +0530

    trying parallex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
+"use client";
 import Image from 'next/image'
 
 import C1 from './components/c1'
 import C2 from './components/c2'
 import C3 from './components/c3'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 export default function Home() {
   return (
     <div>
-       <main className="flex min-h-screen flex-col items-center justify-between ">
+
+       <div className="flex min-h-screen flex-col items-center justify-between ">
       this is the main page
       <p>this is next</p>
-    </main>
+    </div>
       <C1/>
       <C2/>
       <C3/>
