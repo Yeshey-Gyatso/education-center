2c0fd7ca649366fa94dc3f1995c8151cf93eab11
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-17 08:43:21 +0530

    trying parallex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import C1 from './components/c1'
 import C2 from './components/c2'
 import C3 from './components/c3'
@@ -6,6 +7,11 @@ import C3 from './components/c3'
 export default function Home() {
   return (
     <div>
+      <Parallax pages={1} style={{ top: '0', left: '0' }}>
+      <ParallaxLayer offset={0} speed={2.5}>
+        <p>Parallax</p>
+      </ParallaxLayer>
+      </Parallax>
       <C1/>
       <C2/>
       <C3/>
