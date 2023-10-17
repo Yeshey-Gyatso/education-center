ee485f43eb64097d30679f229ddea48c7ff4f8b5
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-17 08:47:33 +0530

    adding some gaps to check the parallex

--- src/app/components/c1.tsx
+++ src/app/components/c1.tsx
@@ -1,10 +1,17 @@
+"use client";
 import React from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 const C1 = () => {
   return (
     <div
     className=' w-auto h-screen bg-orange-50'
-    > this is C1</div>
+    > 
+    <Parallax pages={1} style={{ top: '0', left: '0' }}>
+      <ParallaxLayer offset={0} speed={2.5}>
+        <p>Parallax</p>
+      </ParallaxLayer>
+      </Parallax>this is C1</div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
 import C1 from './components/c1'
 import C2 from './components/c2'
 import C3 from './components/c3'
@@ -7,17 +7,14 @@ import C3 from './components/c3'
 export default function Home() {
   return (
     <div>
-      <Parallax pages={1} style={{ top: '0', left: '0' }}>
-      <ParallaxLayer offset={0} speed={2.5}>
-        <p>Parallax</p>
-      </ParallaxLayer>
-      </Parallax>
+       <main className="flex min-h-screen flex-col items-center justify-between ">
+      this is the main page
+      <p>this is next</p>
+    </main>
       <C1/>
       <C2/>
       <C3/>
     </div>
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   this is the main page
-    // </main>
+   
   )
 }
