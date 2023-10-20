676f42eedebd0216d9d01262107355367d5fa2e9
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-10-20 15:47:31 +0530

    trying some grid designs

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Landing = () => {
   return (
     <div
-    className='bg-neutral-300 z-10 flex justify-center items-center h-full w-screen'>Landing</div>
+    className='bg-neutral-300 z-10  h-full w-screen'>
+        <div className="grid grid-cols-12">
+        <div className="col-span-1 h-36 bg-blue-200 p-4">1</div>
+        <div className="col-span-1 h-36 bg-green-200 p-4">2</div>
+        <div className="col-span-1 h-36 bg-red-200 p-4">3</div>
+        <div className="col-span-1 h-36 bg-yellow-200 p-4">4</div>
+        <div className="col-span-1 h-36 bg-indigo-200 p-4">5</div>
+        <div className="col-span-1 h-36 bg-pink-200 p-4">6</div>
+        <div className="col-span-1 h-36 bg-teal-200 p-4">7</div>
+        <div className="col-span-1 h-36 bg-orange-200 p-4">8</div>
+        <div className="col-span-1 h-36 bg-purple-200 p-4">9</div>
+        <div className="col-span-1 h-36 bg-cyan-200 p-4">10</div>
+        <div className="col-span-1 h-36 bg-gray-200 p-4">11</div>
+        <div className="col-span-1 h-36 bg-amber-800 p-4">12</div>
+        </div>
+        
+        </div>
   )
 }
 
