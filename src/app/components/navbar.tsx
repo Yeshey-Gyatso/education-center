72cc34782b47711a17d2fedea92bdf4bfc109d07
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-10-20 13:07:12 +0530

    changing nav bar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,15 +2,28 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=' w-screen flex  justify-between '>
-      <div>
-        navbar
+    <div className=' w-screen flex  justify-between items-center '>
+      <div className=' p-2'>
+        navbar logo
       </div>
-      <ul className=' flex gap-3'>
-        <li>about</li>
+      <ul className=' flex gap-4 items-center p-2'>
+      <li>home</li>
+        <li>about us</li>
+        <li>courses</li>
         <li>faculty</li>
         <li>gallery</li>
-        <li>student</li>
+        <li>achievement</li>
+        <li>career</li>
+        <li>contact us</li>
+      </ul>
+
+      <ul className=' flex gap-3 items-center p-2'>
+        <li>
+          ig
+          </li>
+        <li>facebook</li>
+        <li>linkedIn</li>
+        <li>yt</li>
       </ul>
       </div>
   )
