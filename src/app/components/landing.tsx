8218f3914d5295e8651238b35e92c9e36c0f0b3e
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-10-21 11:51:35 +0530

    error with next/image occured

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Landing = () => {
   return (
-    <div
-    className='bg-neutral-300 z-10 grid grid-cols-12 h-full w-screen '>
-        
+    <div className=' h-full  w-screen '
+    >
+        <div style={{ position: 'relative' }} className=' z-20'>
+        <Image
+        src="/bg1.jpg" // path relative to the 'public' folder
+        layout="fill"
+        objectFit="cover" // Change to suit your needs
+        quality={100} // Change to suit your needs
+        alt="Background Image"
+        />
+
+        </div>
+        {/* <div className='bg-neutral-300 z-10 grid grid-cols-12 h-full  relative w-screen '>
         <div className="col-span-1 bg-blue-200 p-4"></div>
         <div className="col-span-1 bg-green-200 p-4"></div>
         <div className="col-span-1 bg-red-200 p-4"></div>
@@ -17,6 +27,9 @@ const Landing = () => {
         <div className="col-span-1 bg-cyan-200 p-4"></div>
         <div className="col-span-1 bg-gray-200 p-4"></div>
         <div className="col-span-1 bg-amber-200 p-4"></div>
+
+        </div> */}
+        
     </div>
         
         
