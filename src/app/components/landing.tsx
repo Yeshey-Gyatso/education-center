a08607ee37d369c72d73b5f356208b8f55a46a9c
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-21 21:27:27 +0530

    adding some extra stuffs for landing

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+
 const Landing = () => {
   return (
-    <div className=' h-full  w-screen '
-    >
-        <Image className=' absolute -z-10'
-        src="/bg2.jpg" alt={'image'}    
+    <div className='h-full w-screen relative md:pt-60'>
+      {/* Background image */}
+      <Image
+        src="/wave2.svg"
+        alt="Background Image"
         layout="fill"
-        objectFit="cover"    
-        />
-        {/* <div className='bg-neutral-300 z-10 grid grid-cols-12 h-full  relative w-screen '>
-        <div className="col-span-1 bg-blue-200 p-4"></div>
-        <div className="col-span-1 bg-green-200 p-4"></div>
-        <div className="col-span-1 bg-red-200 p-4"></div>
-        <div className="col-span-1 bg-yellow-200 p-4"></div>
-        <div className="col-span-1 bg-indigo-200 p-4"></div>
-        <div className="col-span-1 bg-pink-200 p-4"></div>
-        <div className="col-span-1 bg-teal-200 p-4"></div>
-        <div className="col-span-1 bg-orange-200 p-4"></div>
-        <div className="col-span-1 bg-purple-200 p-4"></div>
-        <div className="col-span-1 bg-cyan-200 p-4"></div>
-        <div className="col-span-1 bg-gray-200 p-4"></div>
-        <div className="col-span-1 bg-amber-200 p-4"></div>
+        objectFit="cover"
+      />
 
-        </div> */}
-        
+      <div className='bg-transparent grid grid-cols-12 h-full relative w-screen'>
+        <div className="col-span-4 bg-image-container p-4">
+          {/* Use a standard HTML img element for non-background images */}
+          <img className=' rounded-full' src="/P.png" alt='' />
+        </div>
+
+        <div className="col-span-8">
+          <div className=" p-4 pl-72">
+            <div className=' bg-white' />
+            <p className=' text-4xl'>"Your first step towards success"</p>
+          </div>
+
+          <div className="bg-indigo-200 p-4">
+            <p>Text 1</p>
+          </div>
+
+          <div className="bg-pink-200 p-4">
+            <p>Text 2</p>
+          </div>
+        </div>
+      </div>
     </div>
-        
-        
-  )
+  );
 }
 
-export default Landing
+export default Landing;
