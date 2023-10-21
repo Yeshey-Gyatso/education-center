b976c2ed933cde52778dc231e40806bdcd66bcd1
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-21 11:07:49 +0530

    comitted

---
+++ src/app/gallery/gallery.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Gallery = () => {
+  return (
+    <div>
+        <Navbar/>
+        Gallary</div>
+  )
+}
+
+export default Gallery
