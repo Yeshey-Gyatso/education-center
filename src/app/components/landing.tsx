227473b8105e240f5eea4b5e9a1e32dc6d99482f
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-10-21 11:17:31 +0530

    removing numbering from the website

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -5,18 +5,18 @@ const Landing = () => {
     <div
     className='bg-neutral-300 z-10 grid grid-cols-12 h-full w-screen '>
         
-        <div className="col-span-1 bg-blue-200 p-4">1</div>
-        <div className="col-span-1 bg-green-200 p-4">2</div>
-        <div className="col-span-1 bg-red-200 p-4">3</div>
-        <div className="col-span-1 bg-yellow-200 p-4">4</div>
-        <div className="col-span-1 bg-indigo-200 p-4">5</div>
-        <div className="col-span-1 bg-pink-200 p-4">6</div>
-        <div className="col-span-1 bg-teal-200 p-4">7</div>
-        <div className="col-span-1 bg-orange-200 p-4">8</div>
-        <div className="col-span-1 bg-purple-200 p-4">9</div>
-        <div className="col-span-1 bg-cyan-200 p-4">10</div>
-        <div className="col-span-1 bg-gray-200 p-4">11</div>
-        <div className="col-span-1 bg-amber-200 p-4">12</div>
+        <div className="col-span-1 bg-blue-200 p-4"></div>
+        <div className="col-span-1 bg-green-200 p-4"></div>
+        <div className="col-span-1 bg-red-200 p-4"></div>
+        <div className="col-span-1 bg-yellow-200 p-4"></div>
+        <div className="col-span-1 bg-indigo-200 p-4"></div>
+        <div className="col-span-1 bg-pink-200 p-4"></div>
+        <div className="col-span-1 bg-teal-200 p-4"></div>
+        <div className="col-span-1 bg-orange-200 p-4"></div>
+        <div className="col-span-1 bg-purple-200 p-4"></div>
+        <div className="col-span-1 bg-cyan-200 p-4"></div>
+        <div className="col-span-1 bg-gray-200 p-4"></div>
+        <div className="col-span-1 bg-amber-200 p-4"></div>
     </div>
         
         
