7e34ffa9c69b634305790de94c8343526a15ff31
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-22 10:34:23 +0530

    added extra features

---
+++ src/app/contact/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Contact = () => {
+  return (
+    <div>
+        <Navbar/>
+        Contact</div>
+  )
+}
+
+export default Contact
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from './contact'
+
+const ContactPage = () => {
+  return (
+    <div>
+        <Contact/>
+        ContactPage</div>
+  )
+}
+
+export default ContactPage
