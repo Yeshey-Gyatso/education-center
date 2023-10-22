e7c82287b6022db31a2653ad5339e6494c49f5f9
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-22 12:46:22 +0530

    beautifying landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    fetch('/scholar.json')
+    fetch('/wheel.json')
       .then(response => response.json())
       .then(data => setAnimationData(data));
   }, []);
@@ -51,16 +51,18 @@ export default function Home() {
 
         <ParallaxLayer sticky={{ start: 1, end: 3 }} 
         className=' flex justify-start items-center pl-10'>
-          <div className=" h-72 w-auto  flex items-center ">
-        <Lottie animationData={animationData} loop={true} />
+          <div className=" ml-20 w-auto  flex items-center ">
+        <Lottie className='' animationData={animationData} loop={true} />
           </div>
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.5} speed={1.5} 
         className='flex justify-end  pr-5'
         >
-          <div className="bg-purple-400 h-20 w-1/4 border-r-2 flex items-center ">
-            <p>I'm not</p>
+          <div className=" mt-20 rounded-lg shadow-lg bg-teal-400 mr-20 h-2/4 p-4 w-2/4 border-r-2 flex flex-col items-center ">
+          <h1 className=' text-3xl p-5 font-serif'>Our Focus</h1>
+            <p className=' font-sans'>Our primary goal at this UPSC coaching center is to empower and guide aspirants on their journey to becoming successful civil servants. We are committed to providing comprehensive, top-quality education and support to help our students excel in the highly competitive civil services examination. Our mission is to instill in them not just the knowledge and skills necessary to pass the exam, but also the values and ethical principles that are integral to public service. We aspire to create a nurturing and motivating environment that fosters personal growth, critical thinking, and leadership qualities. Ultimately, our aim is to see our students succeed in their pursuit of serving the nation, making a meaningful impact on society, 
+              and upholding the highest standards of governance.</p>
           </div>
           {/* <C1/> */}
         </ParallaxLayer>
