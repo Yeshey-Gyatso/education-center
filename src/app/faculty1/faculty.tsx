31de33da77428ae8970c63b7c3acf15348f98c92
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-28 20:30:49 +0530

    adding faculty

--- src/app/faculty1/faculty.tsx
+++ src/app/faculty1/faculty.tsx
@@ -2,11 +2,55 @@ import React from 'react'
 import Navbar from '../components/navbar'
 
 const Faculty = () => {
+  const faculty=[
+    {
+      id:1,
+      name:"Diken Sharma",
+      DOB:"07-07-1997"
+      pic:"/diken.png",
+      description:"The Official Website of BKYT Literary Organisation of Sikkim "
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+
+  ]
   return (
-    <div>
+    <div className='h-screen w-screen overflow-x-hidden '>
       <Navbar/>
-      Faculty</div>
+    <div className='flex flex-col  h-screen'>
+      <div className='bg-cyan-600 h-40 w-screen flex justify-center items-center '>
+        <h1 className=' text-3xl'>Faculty</h1>
+      </div>
+      <div className=' h-80 w-screen bg-orange-50 flex justify-center items-center '>
+        <p className=' p-20 text-lg'>
+        Our Teachers and mentors possess a diverse set of qualities and skills to engage and educate  students effectively. 
+        They are passionate, experts in subject matter and adaptable. We pride ourselves in approachability and inclusiveness with no student left behind. 
+        These qualities collectively contribute to a positive and effective teaching and learning environment, fostering the academic growth and success of students.
+
+        </p>
+      </div>
+
+      <div className='  p-20  w-screen bg-gray-300 flex flex-col justify-center items-center'>
+      
+      
+      
+
+      </div>
+      
+
+    </div>
+    
+      
+    </div>
   )
 }
+
 
 export default Faculty
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Landing from './components/landing';
-import Contactform from './components/contact';
+
 import Goals from './components/goals';
 import SecondParallex from './components/secondparallex';
 import CoursesLanding from './components/courses';
@@ -68,10 +68,12 @@ export default function Home() {
           <div className=" mt-20 rounded-lg 
           shadow-lg bg-sky-300 h-2/4 p-4 w-2/4  flex flex-col items-center ">
           <h1 className=' text-3xl p-5 font-serif'>Our Vision</h1>
-            <p className=' font-sans'>Pedestal Education is an educational venture to tap into the human potential of young people of Sikkim and to inculcate a competitive spirit among students and aspirants.
-             Civil services and government service has always captured the 
-            interest of the youth. In Sikkim, UPSC civil service exam has garnered a sense of mysticism with very few of Sikkimese clearing such exams and even fewer represented in governance.
-             We hope to provide quality and inclusive guidance and mentorship 
+            <p className=' font-sans'>Pedestal Education is an educational venture to tap into the human potential of young people of Sikkim 
+            and to inculcate a competitive spirit among students and aspirants.
+            Civil services and government service has always captured the 
+            interest of the youth. In Sikkim, UPSC civil service exam has garnered a sense of mysticism 
+            with very few of Sikkimese clearing such exams and even fewer represented in governance.
+            We hope to provide quality and inclusive guidance and mentorship 
             to overcome any obstacle and illuminate a pathway to not only civil service exams but various other government exams. 
             We at Pedestal pride ourselves in our student centric approach and have assembled a well rounded team to help aspirants in any way they require.
             </p>
