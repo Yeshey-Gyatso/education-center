24e4809bce82c4a16a13e30c955b93c87fe04073
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-28 18:50:07 +0530

    updating a few stuffs

--- src/app/courses/courses.tsx
+++ src/app/courses/courses.tsx
@@ -9,8 +9,8 @@ const Courses = () => {
         <div className="flex h-screen">
   <div className="w-1/2 bg-blue-400">
        <div className=' relative pt-10 left-40'>
-       <h1 className=' text-4xl text-gray-700'> Courses Available </h1>
-       <h1 className=' font-light'>are listed below</h1>
+          <h1 className=' text-4xl text-gray-700'> Courses Available </h1>
+          <h1 className=' font-light'>are listed below</h1>
        <div>
         
        </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
         className=' flex justify-start items-center '>
           <div className=" -ml-2 bg-purple-400 h-32 w-2/4 flex items-center ">
             <Lottie className='' animationData={animationData2} loop={true} />
-           <p className=' font-sans text-lg'>"Education is the key that unlocks the doors to a world of possibilities."</p>
+           <p className=' font-sans text-xl'>"Welcome to Pedestal Education"</p>
           </div>
         </ParallaxLayer>
 
@@ -68,9 +68,12 @@ export default function Home() {
           <div className=" mt-20 rounded-lg 
           shadow-lg bg-sky-300 h-2/4 p-4 w-2/4  flex flex-col items-center ">
           <h1 className=' text-3xl p-5 font-serif'>Our Vision</h1>
-            <p className=' font-sans'>Pedestal Education is an educational venture to tap into the human potential of young people of Sikkim and to inculcate a competitive spirit among students and aspirants. Civil services and government service has always captured the 
-            interest of the youth. In Sikkim, UPSC civil service exam has garnered a sense of mysticism with very few of Sikkimese clearing such exams and even fewer represented in governance. We hope to provide quality and inclusive guidance and mentorship 
-            to overcome any obstacle and illuminate a pathway to not only civil service exams but various other government exams. We at Pedestal pride ourselves in our student centric approach and have assembled a well rounded team to help aspirants in any way they require.
+            <p className=' font-sans'>Pedestal Education is an educational venture to tap into the human potential of young people of Sikkim and to inculcate a competitive spirit among students and aspirants.
+             Civil services and government service has always captured the 
+            interest of the youth. In Sikkim, UPSC civil service exam has garnered a sense of mysticism with very few of Sikkimese clearing such exams and even fewer represented in governance.
+             We hope to provide quality and inclusive guidance and mentorship 
+            to overcome any obstacle and illuminate a pathway to not only civil service exams but various other government exams. 
+            We at Pedestal pride ourselves in our student centric approach and have assembled a well rounded team to help aspirants in any way they require.
             </p>
             </div>
     
@@ -101,3 +104,7 @@ export default function Home() {
    </div> 
   )
 }
+function componentDidMount(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
+
