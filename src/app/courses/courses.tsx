3278ec019b8ec74f7df8480d9fd4006eec8409ff
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-10-31 14:45:42 +0530

    adding a few details

--- src/app/courses/courses.tsx
+++ src/app/courses/courses.tsx
@@ -56,6 +56,40 @@ const Courses = () => {
 </div>
 </div>
 
+{/* //2nd course */}
+
+<div className=' p-20 w-screen bg-gray-300 flex flex-row justify-center items-center'>
+<div className='flex flex-col'>
+  <p className='text-lg tracking-wider font-bold '>
+  Optional subject course for UPSC/SPSC
+  </p>
+  <br />
+  <ul className="list-disc text-base ">
+    <li className="mb-2">Duration: 10-month long course to cover General Studies syllabus.</li>
+    <li className="mb-2">Academic Subjects: Course covers core subjects, including History, Geography, Polity, and social studies.
+    This course will also cover UPSC(mains) specific topics.</li>
+    <li className="mb-2">Skill Development: In addition to academic content, foundation courses often focus on essential skills like study techniques and communication.</li>
+    <li className="mb-2">Study Habits and Time Management: Students are taught how to manage their time effectively and develop good study habits.</li>
+    <li className="mb-2">Career Guidance: Counseling on government exam calendar and guide students according to their strength.</li>
+    <li className="mb-2">Assessment and Evaluation: Regular assessments, quizzes, discussion, and answer writing exercises gauge students' progress and understanding.</li>
+    <li className="mb-2">Mentoring and Counseling: Mentoring and counseling to help students navigate their academic journey and address any challenges they may face.</li>
+    <li className="mb-2">Flexible Content: The content of integrated foundation courses may be tailored to the specific needs and goals of the students, whether they are absolute beginners or students who have already been preparing.</li>
+</ul>
+<Link className='text-black bg-transparent px-6 py-3 
+                 my-8 mx-auto flex items-center rounded-md border
+                  border-black hover:scale-110 duration-300'
+                  href={"/contact-us"}
+                  >
+                  contact us
+                </Link>
+
+</div>
+<div className=' mr-20'>
+<img src="clip.png"  className='  rounded-3xl  ' alt="Image Description" />
+        
+      </div>
+</div>
+
     
 
   </div>
