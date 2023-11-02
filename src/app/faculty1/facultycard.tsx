a3bd7bf3961c6e1f3e4f100d1fd9d4112b79405c
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-11-02 11:04:13 +0530

    adding animation on the card

--- src/app/faculty1/facultycard.tsx
+++ src/app/faculty1/facultycard.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {AiFillCloseCircle} from 'react-icons/ai'
 import { FacultyCardProps } from './facultytypes';
 
-export default function FacultyCard({ id, name,Exam1,Exam2,Exam3,Exam4,Exam5, Education, Subject1,Subject2,Subject3,Subject4,Subject5, pic, Motto, showDetails, FacultyDetail,toggleDetails }: FacultyCardProps) {
+export default function FacultyCard({ id, name,Exam1,Exam2,Exam3,Exam4,Exam5, Education, Subject1,Subject2,Subject3,Subject4,Subject5, pic, Motto, showDetails, FacultyDetail,toggleDetails}: FacultyCardProps) {
   return (
-    <div className={`w-full cursor-pointer duration-200 
-    transform hover:scale-105   ${!showDetails ? 'md:w-1/2 ':''} p-4 ${showDetails ? 'fixed inset-0 w-screen h-screen flex items-center justify-center z-20 bg-gray-800 transition-opacity duration-300' : ''}`}>
+    <div className={`w-full cursor-pointer ${!showDetails ? 'md:w-1/2 ':''} p-4
+     ${showDetails ? 'fixed inset-0 w-screen h-screen flex items-center justify-center z-20 bg-gray-800 transition-opacity duration-300' : ''}`}>
       {showDetails ? (
         <div className="bg-white rounded-lg shadow-md relative p-5">
           <button className="absolute top-2 right-2 
@@ -65,8 +65,8 @@ export default function FacultyCard({ id, name,Exam1,Exam2,Exam3,Exam4,Exam5, Ed
         </div>
       ) : (
         <div className="bg-white rounded-lg shadow-md p-4" onClick={() => toggleDetails(id)}>
-          <div className="font-thin hover:font-normal cursor-pointer">
-            <h1 className="text-xl font-semibold">{name}</h1>
+          <div className="font-thin hover:font-normal cursor-pointer ">
+            <h1 className="text-xl font-semibold duration-200  transform hover:border-t-2" >{name}</h1>
           </div>
           <div className="">
             <img src={pic} alt="image" className="mx-auto w-48 h-48 object-cover rounded-full duration-200 transform hover:scale-105" />
