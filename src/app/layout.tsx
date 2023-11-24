ae7c8dceea825e418884e5ffd62ee5c35d7b23bc
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-11-24 12:36:42 +0530

    ok will solve later

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Pedestal Education',
-  description: 'Pedestal Education is a UPSC coaching center based in Sikkim',
+ description: 'Pedestal Education is a UPSC coaching center based in Sikkim',
+ keywords: 'UPSC, coaching, Sikkim',
+ 
 }
 
 export default function RootLayout({
@@ -16,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <Head>
+       <link rel="icon" href="/favicon.ico" />
+     </Head>
       <body className={inter.className}>
         {children}</body>
     </html>
