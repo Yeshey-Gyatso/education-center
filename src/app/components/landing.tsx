49c75f2940c37232f3239d820e765e4ea0a03457
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-11-26 08:02:55 +0530

    landing finished

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -23,7 +23,7 @@ const Landing = () => {
       
 
       <div className='bg-transparent md:grid md:grid-cols-12 h-full relative w-screen'>
-        <div className="md:col-span-5 bg-image-container p-4 ">
+        <div className="col-span-5 bg-image-container p-4 ">
           {/* Use a standard HTML img element for non-background images */}
           <img className=' rounded-full md:h-5/6  lg:-mt-16 lg:ml-20' src="/Ologo.png" alt=''  />
         </div>
@@ -33,7 +33,7 @@ const Landing = () => {
             <p className=' text-center md:text-left lg:text-5xl border-b-8 border-gray-400 cursor-default '>"Welcome to Pedestal Education"</p>
           </div>
             <br /><br />  
-          <div className=" lg:text-2xl md:pl-10 cursor-default ">
+          <div className=" text-sm text-center md:text-left lg:text-2xl md:pl-10 cursor-default ">
           <p>The one-stop solution for clearing competitive exams in Sikkim.
             <br />
             Join us for guidance in UPSC/SPSC/Banking examinations.
@@ -41,12 +41,12 @@ const Landing = () => {
              Let us help you every step of your way in your journey</p>
           </div>
 
-          <div className="md:p-6 md:pl-12 lg:pb-10">
-          <p className=' font-thin'> click here for additional information.</p>
+          <div className=" pb-10 md:p-6 md:pl-12 lg:pb-10">
+          <p className=' hidden md:block  font-thin'> click here for additional information.</p>
           </div>
-          <Link href={"/courses"} className=' bg-gray-200 p-4
+          <Link href={"/courses"} className=' bg-gray-200  p-4
            rounded-full shadow-lg hover:scale-105 
-           transition-transform duration-300 ease-in-out lg:ml-12
+           transition-transform duration-300 ease-in-out ml-10 lg:ml-12
           '>  COURSES</Link> 
           <Link href="https://wa.me/7407383714" className='ml-12 rounded-full shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out bg-gray-200 p-4'>
             WHATSAPP</Link>
