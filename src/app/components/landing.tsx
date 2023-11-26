6210ebe51b51216174722eb47c691da52941bd17
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-11-26 07:57:33 +0530

    making responsive

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -22,15 +22,15 @@ const Landing = () => {
       />
       
 
-      <div className='bg-transparent grid md:grid-cols-12 h-full relative w-screen'>
-        <div className="col-span-5 bg-image-container p-4 ">
+      <div className='bg-transparent md:grid md:grid-cols-12 h-full relative w-screen'>
+        <div className="md:col-span-5 bg-image-container p-4 ">
           {/* Use a standard HTML img element for non-background images */}
-          <img className=' rounded-full h-5/6  lg:-mt-16 lg:ml-20' src="/Ologo.png" alt=''  />
+          <img className=' rounded-full md:h-5/6  lg:-mt-16 lg:ml-20' src="/Ologo.png" alt=''  />
         </div>
 
-        <div className="col-span-7">
+        <div className="md:col-span-7">
           <div className=" ">
-            <p className=' lg:text-5xl border-b-8 border-gray-400 cursor-default '>"Welcome to Pedestal Education"</p>
+            <p className=' text-center md:text-left lg:text-5xl border-b-8 border-gray-400 cursor-default '>"Welcome to Pedestal Education"</p>
           </div>
             <br /><br />  
           <div className=" lg:text-2xl md:pl-10 cursor-default ">
