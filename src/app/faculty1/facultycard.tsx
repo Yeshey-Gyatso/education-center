24a7cc0ab8e18a3c3a85210baadb93d8351c876f
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-11-28 23:18:56 +0530

    completed faculty

--- src/app/faculty1/facultycard.tsx
+++ src/app/faculty1/facultycard.tsx
@@ -7,13 +7,13 @@ export default function FacultyCard({ id, name,Exam1,Exam2,Exam3,Exam4,Exam5, Ed
     <div className={`w-full cursor-pointer ${!showDetails ? 'md:w-1/2 ':''} p-4
      ${showDetails ? 'fixed inset-0 w-screen h-screen flex items-center justify-center z-20 bg-gray-800 transition-opacity duration-300' : ''}`}>
       {showDetails ? (
-        <div className="bg-white rounded-lg shadow-md relative p-5">
+        <div className="flex flex-col h-full bg-white rounded-lg shadow-md relative p-5 overflow-y-auto gap-6">
           <button className="absolute top-2 right-2 
            text-gray-500 cursor-pointer duration-200 transform hover:scale-125" 
            onClick={() => toggleDetails(id)}>
           <AiFillCloseCircle/>
           </button>
-          <div className="font-thin hover:font-normal cursor-pointer">
+          <div className="w-full  font-thin hover:font-normal cursor-pointer">
             <h1 className="text-xl font-semibold">{name}</h1>
           </div>
           <div className="">
@@ -66,14 +66,14 @@ export default function FacultyCard({ id, name,Exam1,Exam2,Exam3,Exam4,Exam5, Ed
           </div>
         </div>
       ) : (
-        <div className="bg-white rounded-lg shadow-md p-4" onClick={() => toggleDetails(id)}>
+        <div className=" flex flex-col justify-center px-6 py-6 bg-white rounded-lg shadow-md  space-y-8 lg:min-h-[400px]   " onClick={() => toggleDetails(id)}>
           <div className="font-thin hover:font-normal cursor-pointer ">
-            <h1 className="text-xl font-semibold duration-200  transform hover:border-t-2" >{name}</h1>
+            <h1 className="text-xl text-center font-semibold duration-200  transform hover:border-t-2" >{name}</h1>
           </div>
           <div className="">
             <img src={pic} alt="image" className="mx-auto w-48 h-48 object-cover rounded-full duration-200 transform hover:scale-105" />
           </div>
-          <div className="mt-4 text-gray-600"> 
+          <div className="mt-4 text-gray-600 text-center"> 
           {FacultyDetail}
           </div>
         </div>
